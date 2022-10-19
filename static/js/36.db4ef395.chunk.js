9a6fef62e4b5be66548dc3bf3ca9b45ad14a75dd
Korobass87 <88404829+Korobass87@users.noreply.github.com>
2022-10-20 02:41:00 +0300

    Updates

---
+++ static/js/36.db4ef395.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp1=this.webpackJsonp1||[]).push([[36],{506:function(t,n,s){"use strict";s.r(n);s(0);var c=s(4);n.default=function(){return Object(c.jsx)("div",{children:"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0438"})}}}]);
+//# sourceMappingURL=36.db4ef395.chunk.js.map
